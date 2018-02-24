7331108020295f6632ba4ce0a36a3de12dad401b
Justin Kotalik <jukotali@microsoft.com>
2018-02-23 17:45:54 -0800

    testing

--- test/AspNetCoreModuleTests/stdafx.h
+++ test/AspNetCoreModuleTests/stdafx.h
@@ -5,9 +5,45 @@
 
 #pragma once
 
-#include "targetver.h"
-
 // Headers for CppUnitTest
 #include "CppUnitTest.h"
 
-#include "..\..\src\AspNetCore\Src\precomp.hxx"
+#include <windows.h>
+#include <atlbase.h>
+#include <pdh.h>
+#include <vector>
+#include <Shlobj.h>
+#include <httpserv.h>
+#include <winhttp.h>
+#include <httptrace.h>
+#include <cstdlib>
+#include <wchar.h>
+#include <io.h>
+#include <stdio.h>
+
+#include <hashfn.h>
+#include <hashtable.h>
+#include "stringa.h"
+#include "stringu.h"
+#include "dbgutil.h"
+#include "ahutil.h"
+#include "multisz.h"
+#include "multisza.h"
+#include "base64.h"
+#include <listentry.h>
+#include <datetime.h>
+#include <reftrace.h>
+#include <acache.h>
+#include <time.h>
+
+#include "..\..\src\CommonLib\hostfxr_utility.h"
+#include "..\..\src\CommonLib\environmentvariablehash.h"
+#include "..\..\src\CommonLib\aspnetcoreconfig.h"
+#include "..\..\src\CommonLib\hostfxr_utility.h"
+#include "..\..\src\CommonLib\application.h"
+#include "..\..\src\CommonLib\utility.h"
+#include "..\..\src\CommonLib\debugutil.h"
+#include "..\..\src\CommonLib\requesthandler.h"
+#include "..\..\src\CommonLib\resources.h"
+#include "..\..\src\CommonLib\aspnetcore_msg.h"
+
